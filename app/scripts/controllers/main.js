583013506689617b96edf8a332731806c84932ab
matthewmborda <matthewmborda@gmail.com>
2016-08-03 17:30:04 -0400

    Test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the workspaceApp
  */
 angular.module('workspaceApp')
-  .controller('MainCtrl', function ($scope, current) {
-    $scope.current = current.query();
-  $scope.refreshCurrent = function(){
-        $scope.current = current.query({
-            movieSearch: $scope.movieName       });
+  .controller('MainCtrl', function ($scope, moviesearch) {
+//     $scope.current = current.query();
+//   $scope.refreshCurrent = function(){
+//         $scope.current = current.query({
+//             movieSearch: $scope.movieName       });
+//     };
+      $scope.moviesearch = moviesearch.find();
+
+    $scope.findMovies = function(){
+        $scope.moviesFound = moviesearch.find({
+            query: $scope.movieSearch
+        });
+        $scope.searchQuery = $scope.movieSearch;
     };
   });
